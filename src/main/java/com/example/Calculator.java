05a73b37e66f3c7ffd2e1bc0281291ed2d8924b0
mounirammam <ammammounir@gmail.com>
2025-12-05 10:02:11 -0500

    broke the code

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -7,11 +7,11 @@ public class Calculator {
     }
 
     public double subtract(double a, double b) {
-        return a - b;
+        return a + b;
     }
 
     public double multiply(double a, double b) {
-        return a * b;
+        return a / b;
     }
 
     public double divide(double a, double b) {
